c211f09b945f9e140b11bda71f7136d73b411348
seymakara <seymakara88@gmail.com>
2019-01-30 15:04:39 -0800

    create Event and App component

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,34 @@
+import React, { Component } from 'react';
+import { ReactDOM } from 'react-dom';
+import axios from 'axios';
+import Event from './components/Event.jsx';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div style={{ margin: 20 }} >
+        <h1>Historical Event Finder</h1>
+        <input
+          type="text"
+          className="form-control"
+          id="eventInput"
+          style={{ width: 300, marginBottom: 20 }}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary"
+          style={{ marginBottom: 20 }}
+        >
+          Submit
+        </button>
+        <Event />
+      </div>
+    );
+  }
+}
 
+export default App;
---
+++ challenge_1/client/components/Event.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Event = () => {
+  return (
+    <div className='eventItem'>
+      <div>
+        <h2>Category</h2>
+      </div>
+      <div>
+        <h2>Date</h2>
+      </div>
+      <div>
+        <h2>Description</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default Event;
