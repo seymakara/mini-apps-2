28689272065e7f1ed9ecbb1d46b8dcf25a581e0f
seymakara <seymakara88@gmail.com>
2019-01-30 17:07:08 -0800

    complete paginated get request

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 import { ReactDOM } from 'react-dom';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import EventList from './components/EventList.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      searchKeyword: '',
+      currentPage: 1,
+      numberOfPages: 0,
+      eventData: []
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.getEvents = this.getEvents.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
   }
+
+  getEvents() {
+    let { searchKeyword, currentPage } = this.state;
+    axios.get(`/events?q=${searchKeyword}`)
+      .then((response) => {
+        console.log(this);
+        this.setState({
+          numberOfPages: response.data.length / 10
+        }, () => {
+          axios.get(`/events?q=${searchKeyword}&_page=${currentPage}`)
+            .then(response => {
+              console.log('get req success: ', response);
+              this.setState({
+                currentPage: 1,
+                result: response.data
+              });
+            })
+            .catch(err => {
+              console.log('get req pagination error: ', err);
+            });
+        });
+      })
+      .catch(error => {
+        console.log('get req pagination error:', error);
+      });
+  }
+
+  handleInputChange(e) {
+    let searchKeyword = e.target.value
+    this.setState({ searchKeyword });
+  }
+
+  handlePageChange() {
+
+  }
+
+
+
   render() {
     return (
       <div style={{ margin: 20 }} >
@@ -17,15 +64,32 @@ class App extends Component {
           className="form-control"
           id="eventInput"
           style={{ width: 300, marginBottom: 20 }}
+          onChange={this.handleInputChange}
         />
         <button
           type="submit"
           className="btn btn-primary"
           style={{ marginBottom: 20 }}
+          onClick={this.getEvents}
         >
-          Submit
+          Search
         </button>
-        <EventList />
+        <EventList eventData={this.state.eventData} />
+        <ReactPaginate
+          className="pagination"
+          pageCount={this.state.numberOfPages}
+          pageRangeDisplayed={5}
+          forcePage={0}
+          marginPagesDisplayed={1}
+          previousLabel={"< Previous"}
+          nextLabel={"Next >"}
+          onPageChange={(currentPage) => {
+            console.log('currentPage', currentPage)
+            this.setState({
+              currentPage: currentPage.selected + 1
+            }, this.handlePageChange);
+          }}
+        />
       </div>
     );
   }
