957211f51174bebf58f070d28a67dbb45d671aba
seymakara <seymakara88@gmail.com>
2019-01-30 18:11:46 -0800

    render data on single event page

--- challenge_1/client/components/Event.jsx
+++ challenge_1/client/components/Event.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-const Event = () => {
+const Event = (props) => {
+  let { category2, date, description } = props.event;
   return (
     <div className='eventItem'>
       <div>
         <h3>Category</h3>
+        <p>{category2}</p>
       </div>
       <div>
         <h3>Date</h3>
+        <p>{date}</p>
       </div>
       <div>
         <h3>Description</h3>
+        <p>{description}</p>
       </div>
+      <div style={{ borderBottomWidth: 1, borderBottomStyle: 'solid', marginBottom: 10 }}></div>
 
     </div>
   );
