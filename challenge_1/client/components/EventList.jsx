79781410f5cebbc266cd20268034c5eba5703ac5
seymakara <seymakara88@gmail.com>
2019-01-30 18:12:43 -0800

    fix return error

--- challenge_1/client/components/EventList.jsx
+++ challenge_1/client/components/EventList.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Event from './Event.jsx';
 
 const EventList = (props) => {
-  let events = props.eventData;
-  events.map((event, index) => {
-    return (
-      <Event event={event} key={index} />
-    )
-  });
+  return (
+    <div>
+      {props.eventData.map((event, index) => {
+        return <Event event={event} key={index} />;
+      })}
+    </div>
+  )
 }
 
 export default EventList;
