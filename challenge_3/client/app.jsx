1734a5d8de07722743f98a74267e789cfb2a273e
seymakara <seymakara88@gmail.com>
2019-02-01 23:15:52 -0800

    complete pin selection functionality

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,22 +7,41 @@ class App extends Component {
     super(props);
     this.state = {
       scores: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],],
-      currentFrame: 0
+      currentFrame: 0,
+      pinsLeft: 10,
+      ballsThrown: 0,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    console.log(e.target.value)
+    const { pinsLeft, currentFrame } = this.state;
+    let { ballsThrown } = this.state;
+    ballsThrown += 1;
+    if (ballsThrown % 2) {
+      this.setState({
+        ballsThrown,
+        pinsLeft: pinsLeft - e.target.value,
+      });
+    } else {
+      this.setState({
+        currentFrame: currentFrame + 1,
+        ballsThrown,
+        pinsLeft: 10,
+      });
+    }
   }
 
+  // TODO handle bowling logic
+
+
   render() {
-    let { number, scores } = this.state;
+    let { pinsLeft, scores } = this.state;
     return (
       <div>
         <NumberPad
+          pinsLeft={pinsLeft}
           handleClick={this.handleClick}
-          number={number}
         />
         <ScoreBoard scores={scores} />
       </div>
--- challenge_3/client/components/NumberPad.jsx
+++ challenge_3/client/components/NumberPad.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Number from './Number.jsx';
+import _ from 'lodash';
 
-const NumberPad = ({ handleClick }) => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const NumberPad = ({ handleClick, pinsLeft }) => {
   return (
     <div style={{ marginBottom: 20 }}>
       {
-        numbers.map(number => {
+        _.times(pinsLeft, number => {
           return (
             <Number
               number={number}
