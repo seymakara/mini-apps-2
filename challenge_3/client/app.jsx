27997dbd6273eb710bb27a0d6fb846653d640590
seymakara <seymakara88@gmail.com>
2019-02-06 01:27:32 -0800

    refactoring

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';;
 import NumberPad from './components/NumberPad.jsx';
 import ScoreBoard from './components/ScoreBoard.jsx';
-import scoreReducer from './utils/scoreReducer';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scores: { 0: [0, 0], 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0], 9: [0, 0] },
-      currentFrame: 0,
-      pinsLeft: 11,
       ballsUsed: 0,
       frameTotal: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      pinsLeft: 11,
+      scores: { 0: [0, 0], 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0], 9: [0, 0] },
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    let { pinsLeft, currentFrame, ballsUsed } = this.state;
+    let { ballsUsed, frameTotal, pinsLeft, scores} = this.state;
+
+    let currentFrame = Math.floor(ballsUsed / 2);
     ballsUsed += 1;
-    let scores = { ...this.state.scores };
-    let frameTotal = [...this.state.frameTotal];
-    
+
+    // Define strike & spare flags
     let strike = false;
     let spare = false;
     if (currentFrame > 0 && scores[currentFrame - 1][0] === 10) {
@@ -31,27 +30,31 @@ class App extends Component {
       spare = true;
     }
     
+
+    // Scoring operations
     if (ballsUsed % 2 === 1) {
       scores[currentFrame][0] = Number(e.target.value);
-      if (spare){
+      
+      if(spare){
         frameTotal[currentFrame - 1]+=Number(e.target.value)
       }
+      
       this.setState({
         ballsUsed,
         pinsLeft: pinsLeft - e.target.value,
         scores,
-        frameTotal,
       });
 
 
     } else {
       scores[currentFrame][1] = Number(e.target.value);
+      
       if (strike){
         frameTotal[currentFrame - 1]+=scores[currentFrame][0]
         frameTotal[currentFrame - 1]+=scores[currentFrame][1]
       }
 
-      if(currentFrame > 0){
+      if (currentFrame > 0){
         frameTotal[currentFrame] = frameTotal[currentFrame-1] + scores[currentFrame][0] + scores[currentFrame][1]
       }
       else {
@@ -59,7 +62,6 @@ class App extends Component {
       }
 
       this.setState({
-        currentFrame: currentFrame + 1,
         ballsUsed,
         pinsLeft: 11,
         scores,
